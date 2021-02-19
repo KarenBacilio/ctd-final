c3b0cb5fd8f71c81fd6db0499a5406dbc2ec4c1e
Karen Bacilio <karen.bacilio@gmail.com>
2021-02-18 23:38:29 -0500

    console logs connected html and js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-//JS file for my webpage
-console.log("It is connected correctly")
-// document.addEventListener('DOMContentLoaded', () =>{
-//     alert("Welcome to the Treeist Website!")
-// })
-
-console.log("checking something with my project")
+console.log("connected successfully")
